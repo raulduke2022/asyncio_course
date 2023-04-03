fc21619daea8a9a4d04db97a570bdf0182a115ad
raulduke2022 <sergeytersahakyan@gmail.com>
2023-04-03 23:37:42 +0300

    new tests

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import asyncio
-
-from util import async_timed
+from util import async_timed, delay
+import requests
+import functools
 
 
 @async_timed()
-async def delay(delay_seconds: int) -> int:
-    print(f'засыпаю на {delay_seconds} с')
-    await asyncio.sleep(delay_seconds)
-    print(f'сон в течение {delay_seconds} с закончился')
-    return delay_seconds
+async def youtube() -> None:
+    main_loop = asyncio.get_event_loop()
+    main_loop.run_in_executor(None, functools.partial(requests.get, url="https://www.youtube.com/"))
+
+
 @async_timed()
 async def main():
-    task_one = asyncio.create_task(delay(2))
-    task_two = asyncio.create_task(delay(3))
-    await task_one
-    await task_two
+    tasks = []
+    for i in range(100):
+        tasks.append(asyncio.create_task(youtube()))
 
+    for task in tasks:
+        await task
 
-asyncio.run(main())
+asyncio.run(main(), debug=True)
